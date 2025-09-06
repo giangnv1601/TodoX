2b48116cc6531e833c01fcc9f5e5898df0d1e2b0
Nguyen Van Giang <giang16012405@gmail.com>
2025-09-06 11:20:43 +0700

    UI

---
+++ backend/src/server.js
@@ -0,0 +1,24 @@
+import express, { request, response } from 'express';
+import tasksRoute from "./routes/tasksRouters.js"
+import { connectDB } from './config/db.js';
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5001;
+
+const app = express();
+
+
+
+app.use(express.json());
+
+app.use("/api/tasks", tasksRoute);
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server bắt đầu trên cổng ${PORT}`);
+  })
+});
+
+
